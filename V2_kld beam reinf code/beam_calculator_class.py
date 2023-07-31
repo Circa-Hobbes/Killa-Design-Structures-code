9c490181011b267a2e3acc19f9bd4675e6e83594
Circa-Hobbes <130729240+Circa-Hobbes@users.noreply.github.com>
2023-07-31 16:00:14 +0400

    07_31_23

--- V2_kld beam reinf code/beam_calculator_class.py
+++ V2_kld beam reinf code/beam_calculator_class.py
@@ -1,5 +1,6 @@
 # import math
-# import numpy as np
+import numpy as np
+import pandas as pd
 
 
 class Beam:
@@ -7,15 +8,77 @@ class Beam:
     reinforcement schedule.
     """
 
-    def __init__(self, id):
+    def __init__(self, df, id, width, depth, pos_flex_combo, neg_flex_combo):
         """Begin by initializing attributes that define the makeup of a reinforced
         concrete beam.
         """
         self.id = id
+        self.df = df.loc[df["Unnamed: 1"] == id]
+        self.width = width
+        self.depth = depth
+        self.pos_flex_combo = pos_flex_combo
+        self.neg_flex_combo = neg_flex_combo
 
-    def obtain_id(self):
-        """This method loops through the third row of each spreadsheet to obtain a list
-        of ETABS ID's.
+    def __str__(self):
+        """Create a string describing the attributes of each instantiated beam.
+
+        Returns:
+            _type_: A string of the attributes of a beam.
         """
-        etabs_id = self.id.iloc[::3]
-        return etabs_id
+        return f"""ETABS Beam ID: {self.id}
+Width: {self.width}mm 
+Depth: {self.depth}mm
+Positive Flexural Combo: {self.pos_flex_combo}
+Negative Flexural Combo: {self.neg_flex_combo}"""
+
+    @staticmethod
+    def get_width(width):
+        """This function cleans and retrieves the relevant width of the beam.
+
+        Args:
+            width (_type_): Width in column of dataframe to clean and get width of beam.
+        """
+        width_list = list(width)
+        width_list = [el.lower() for el in width_list]
+        excluded_values = [
+            "p",
+            "t",
+            "b",
+            "-",
+            "_",
+            "c",
+            "/",
+        ]
+        v1_width_list = [ex for ex in width_list if ex not in excluded_values]
+        index_list = v1_width_list.index("x")
+        v2_width_list = v1_width_list[:index_list]
+        true_width = "".join(v2_width_list)
+        return int(true_width)
+
+    @staticmethod
+    def get_depth(depth):
+        """This function cleans and retrives the relevant depth of the beam.
+
+        Args:
+            depth (_type_): Depth in column of dataframe to clean and get depth of beam.
+        """
+        depth_list = list(depth)
+        depth_list = [el.lower() for el in depth_list]
+        excluded_values = ["p", "t", "b", "-", "_", "c", "/"]
+        v1_depth_list = [ex for ex in depth_list if ex not in excluded_values]
+        index_list = v1_depth_list.index("x")
+        v2_depth_list = v1_depth_list[1 + index_list : -4]
+        true_depth = "".join(v2_depth_list)
+        return int(true_depth)
+
+    @staticmethod
+    def check_flex_combo(combo_list):
+        """This function checks if any of the flexural combos in the list is overstressed.
+
+        Args:
+            combo_list (list of string): Checks each flexural combo in the list.
+
+        Returns:
+            list of string: Returns "True" for each overstressed combo and "False" for each not overstressed.
+        """
+        return ["True" if combo == "O/S" else "False" for combo in combo_list]
--- V2_kld beam reinf code/v2_beam_reinf_code.py
+++ V2_kld beam reinf code/v2_beam_reinf_code.py
@@ -10,18 +10,11 @@ import tkinter as tk
 import importlib
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
-# importlib.reload(rebar_func)
 
-
-# Create an instance of the Beam class for when the script is run, not when the module
-# is imported.
-def create_instance():
-    beam = Beam()
+# Create all instances, i.e. etabs id's of the Beam class when the script is run.
+def create_instance(df, id, width, depth, pos_flex_combo, neg_flex_combo):
+    beam = Beam(df, id, width, depth, pos_flex_combo, neg_flex_combo)
     return beam
-
-
-if __name__ == "__main__":
-    instance = create_instance()
 
 
 # Create function to import ETABS beam design extraction spreadsheet.
@@ -48,6 +41,54 @@ initial_shear_df = initial_shear_df.drop([0, 1])
 initial_flexural_df = initial_flexural_df.reset_index(drop=True)
 initial_shear_df = initial_shear_df.reset_index(drop=True)
 
-# Slice through flexural dataframe to obtain all the ETAB beam IDs.
-flexural = Beam(initial_flexural_df)
-e_id = flexural.obtain_id()
+# Begin manipulation and cleaning of dataframe
+if __name__ == "__main__":
+    # Slice through the flexural df and get the etabs id.
+    e_ids = initial_flexural_df["Unnamed: 1"].iloc[::3]
+
+    # Slice through the flexural df and get the cleaned width.
+    beam_widths = initial_flexural_df["Unnamed: 3"].iloc[::3].apply(Beam.get_width)
+
+    # Slice through the flexural df and get the cleaned depth.
+    beam_depths = initial_flexural_df["Unnamed: 3"].iloc[::3].apply(Beam.get_depth)
+
+    # Take each beam's positive flexural combo and put it in a list within a list.
+    positive_combo_list = initial_flexural_df["Unnamed: 9"].tolist()
+    nested_pos_combo_list = [
+        positive_combo_list[i : i + 3] for i in range(0, len(positive_combo_list), 3)
+    ]
+    # Repeat same process as positive flexural combo for negative flexural combo.
+    negative_combo_list = initial_flexural_df["Unnamed: 6"].tolist()
+    nested_neg_combo_list = [
+        negative_combo_list[i : i + 3] for i in range(0, len(negative_combo_list), 3)
+    ]
+
+    # Take the nested list and return OK or O/S as a string in a list.
+    nested_pos_combo_list = [
+        "O/S"
+        if any(str(element).strip().lower() in ["o/s", "nan"] for element in sublist)
+        else "OK"
+        for sublist in nested_pos_combo_list
+    ]
+    nested_neg_combo_list = [
+        "O/S"
+        if any(str(element).strip().lower() in ["o/s", "nan"] for element in sublist)
+        else "OK"
+        for sublist in nested_neg_combo_list
+    ]
+
+    # Slice through the flexural df and retrieve whether is it overstressed in positive combo.
+    positive_flex_combo = Beam.check_flex_combo(nested_pos_combo_list)
+
+    # Slice through the flexural df and retrieve whether it is overstressed in negative combo.
+    negative_flex_combo = Beam.check_flex_combo(nested_neg_combo_list)
+
+    # Call create_instance function and create instances of all etabs ids.
+    beam_instances = [
+        create_instance(
+            initial_flexural_df, e_id, width, depth, pos_flex_combo, neg_flex_combo
+        )
+        for e_id, width, depth, pos_flex_combo, neg_flex_combo in zip(
+            e_ids, beam_widths, beam_depths, positive_flex_combo, negative_flex_combo
+        )
+    ]
