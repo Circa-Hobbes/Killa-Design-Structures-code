60f1b3d0c60ba473bf92aaf83f09c6ff2057ebe3
Circa-Hobbes <130729240+Circa-Hobbes@users.noreply.github.com>
2023-10-26 16:17:01 +0400

    102623

--- unit_testing/test_beam_class.py
+++ unit_testing/test_beam_class.py
@@ -1,13 +1,17 @@
 import sys
 
 sys.path.append(
-    "C:/Users/adnan.a/OneDrive - Killa Design/Documents/GitHub/Killa-Design-Structures-code/unit_testing"
+    r"C:/Users/adnan.a/OneDrive - Killa Design/Documents/GitHub/Killa-Design-Structures-code/unit_testing"
+)
+sys.path.append(
+    r"C:\Users\adnan.a\OneDrive - Killa Design\Documents\GitHub\Killa-Design-Structures-code\V2_kld beam reinf code"
 )
 
 from beam_calculator_class import Beam
 
 
 def test_side_face_string():
+    """Note: for attributes with lists, index 0 is left, index 1 is middle, and index 2 is right."""
     test_beam = Beam(
         id=None,
         width=900,
@@ -23,6 +27,9 @@ def test_side_face_string():
         req_torsion_reinf=[0, 0, 0],
     )
 
+    # Get the effective depth by multiplying the depth by 0.8.
+    test_beam.get_eff_depth()
+    # Get the longitudinal rebar count.
     test_beam.get_long_count()
 
     # Split the torsion reinforcement to the top and bottom rebar if the depth <= 600mm.
@@ -38,9 +45,31 @@ def test_side_face_string():
     # Calculate the residual rebar obtained from the provided against the required.
     test_beam.get_residual_rebar()
 
+    # Calculate the required shear legs based on the beams width.
+    test_beam.get_shear_legs()
+
+    # Calculate the total required shear reinforcement including shear and torsion.
+    test_beam.get_total_shear_req()
+
+    # Calculate the provided shear reinforcement string and area.
+    test_beam.get_shear_string()
+    test_beam.get_shear_area()
+
+    # Check and replace if necessary the maximum longitudinal shear spacing against Clause 18.4.2.4 of ACI 318-19.
+    test_beam.get_min_shear_long_spacing()
+    test_beam.modify_shear_reinf()
+
+    # Grab the index of the shear reinforcement with the highest area.
+    # beam.get_index_for_shear_reinf()
+
     # Calculate the allowable side face clear space in beams which have a depth greater than 600mm.
     test_beam.get_side_face_clear_space()
 
+    # Calculate the provided side face reinforcement string and area.
     test_beam.get_side_face_string()
+    test_beam.get_side_face_area()
+
+    # Grab the index of the side face reinforcement with the highest area.
+    test_beam.get_index_for_side_face_reinf()
 
     assert test_beam.flex_top_left_rebar_string == "Overstressed. Please re-assess"
